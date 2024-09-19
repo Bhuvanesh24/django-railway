d697a47f16c44fc304a7a756b795623d761a3eb0
Bhuvanesh24 <bhuvaneshwaran813@gmail.com>
2024-09-19 22:53:06 +0530

    Initial Commit

---
+++ forecast/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from .models import Reservoir
+
+
+@admin.register(Reservoir)
+class ResorvoirDataAdmin(admin.ModelAdmin):
+    list_display = (
+        'reservoir_name', 'basin', 
+        'year', 'month', 'full_reservoir_level', 'live_capacity_frl', 
+        'storage', 'level'
+    )
